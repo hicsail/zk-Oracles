2dbc1be468955d66d3453fbb7c55046427cf30b0
Harunobu Ishii <harunobuishii547@gmail.com>
2024-02-23 10:01:53 -0500

    Dockerfile updated to run three containers run simultaneously

---
+++ run_containers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run --platform linux/amd64 -dit --name zk-copyright hicsail/zk-copyright:main
+docker run --platform linux/amd64 -dit --name zk-apple-psi hicsail/zk-applepsi:main
+docker run --platform linux/amd64 -dit --name zk-dp hicsail/zk-dp:main
